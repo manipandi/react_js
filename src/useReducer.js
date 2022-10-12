b0e81ef1aacda78b42da162bce36fda1cca54784
manipandi <manirathnam.pandi@anywhere.co>
2022-10-12 22:16:59 +0530

    feat: added code for reducer changes

--- src/useReducer.js
+++ src/useReducer.js
@@ -28,3 +28,28 @@ const reducer = (state, action) => {
       return state;
   }
 };
+
+function Todos() {
+    const [todos, dispatch] = useReducer(reducer, initialTodos);
+  
+    const handleComplete = (todo) => {
+      dispatch({ type: "COMPLETE", id: todo.id });
+    };
+  
+    return (
+      <>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.complete}
+                onChange={() => handleComplete(todo)}
+              />
+              {todo.title}
+            </label>
+          </div>
+        ))}
+      </>
+    );
+  }
