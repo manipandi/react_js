004285f6f0d3d947aa47a5fbf4307f8b9f3cc1ab
manipandi <manirathnam.pandi@anywhere.co>
2022-12-20 17:22:27 +0530

    feat: added class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from 'react';
 import './App.css';
 import UseStateExample from './UseState'
 
-function App() {
-  return (
-    <div className="App">
-     <UseStateExample/>
-    </div>
-  );
+class TestClasss extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:"yogesh"
+    }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if(props.name) {
+      return { name:props.name}
+    } else {
+      return { name: "default name"}
+    }
+  }
+
+  render() {
+    return <>
+    <p>User name is :: {this.state.name}</p>
+    </>
+  }
 }
 
-export default App;
+export default TestClasss;
